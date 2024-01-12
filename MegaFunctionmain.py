3d12c1081caac0bbc9112a1e7e89ffb7cbf48987
Михаил <117997375+MihailSL@users.noreply.github.com>
2024-01-12 18:25:53 +0200

    Create MegaFunctionmain.py

---
+++ MegaFunctionmain.py
@@ -0,0 +1,13 @@
+from __future__ import annotations  # for Python 2.X
+import random
+
+
+def unusual_printing(word: str) -> str:
+    """Returns word+exlamation"""
+
+    exclamations = ["!", "?", ".", ",", "?!", "!?", "!!!", ";", "???", ":", "..."]
+    return f"{word}{random.choice(exclamations)}"
+
+
+if __name__ == "__main__":
+    print(unusual_printing("Hello world"))
